68a146ed47e649875f7943bd37f28bf81a055f4a
Maxime Rainville <maxime@silverstripe.com>
2019-07-29 13:01:30 +1200

    Better handle edge cases where ElementBlogPosts targets a missing blog page

--- tests/Elements/ElementBlogPostsTest.php
+++ tests/Elements/ElementBlogPostsTest.php
@@ -4,9 +4,12 @@ namespace Dynamic\Elements\Blog\Elements\Tests;
 
 use Dynamic\Elements\Blog\Elements\ElementBlogPosts;
 use SilverStripe\Blog\Model\Blog;
+use SilverStripe\Blog\Model\BlogCategory;
 use SilverStripe\Blog\Model\BlogPost;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\SS_List;
 
 class ElementBlogPostsTest extends SapphireTest
 {
@@ -58,6 +61,45 @@ class ElementBlogPostsTest extends SapphireTest
     public function testGetPostsList()
     {
         $object = $this->objFromFixture(ElementBlogPosts::class, 'one');
-        $this->assertEquals($object->Blog()->getBlogPosts()->limit($object->Limit), $object->getPostsList());
+        $this->compareList(
+            $object->Blog()->getBlogPosts()->limit($object->Limit),
+            $object->getPostsList(),
+            'Should only return blog post assign to blog page'
+        );
+
+        $object = $this->objFromFixture(ElementBlogPosts::class, 'targetCategory');
+        $category = $this->objFromFixture(BlogCategory::class, 'category');
+        $this->compareList(
+            $category->BlogPosts()->limit($object->Limit),
+            $object->getPostsList(),
+            'Should only return blog post assign to blog category'
+        );
+
+        $object = $this->objFromFixture(ElementBlogPosts::class, 'noTarget');
+        $this->compareList(
+            BlogPost::get()->limit($object->Limit),
+            $object->getPostsList(),
+            'Should return all blog posts'
+        );
+
+        $object = $this->objFromFixture(ElementBlogPosts::class, 'badTarget');
+        $this->compareList(
+            BlogPost::get()->limit($object->Limit),
+            $object->getPostsList(),
+            'When ElementalBlogPost is misconfigured shoudl return all blog post'
+        );
+    }
+
+    /**
+     * Compare entries in $expected to entries in actual
+     * @param DataList $expected
+     * @param DataList $actual
+     * @param string $message
+     */
+    private function compareList(DataList $expected, DataList $actual, $message='')
+    {
+        $expectedArray = $expected->map('ID', 'ClassName')->toArray();
+        $actualArray = $expected->map('ID', 'ClassName')->toArray();
+        $this->assertEquals($expectedArray, $actualArray, $message);
     }
 }
