a31cd7b38e023ced99cdc2d2cfc545d6c16c476b
Jason Irish <jirish@dynamicagency.com>
2025-11-17 19:32:54 -0600

    Fix SS6 namespace migrations
    - PaginatedList: SilverStripe\ORM\PaginatedList → SilverStripe\Model\List\PaginatedList
    - ArrayList: SilverStripe\ORM\ArrayList → SilverStripe\Model\List\ArrayList
    - Remove framework requirement (resolved by installer)
    - Update blog requirement to ^5.1
    
    Note: Tests will fail due to widget-related code. Widget removal is deferred to a separate PR.

--- tests/Fake/PageController.php
+++ tests/Fake/PageController.php
@@ -4,8 +4,8 @@ namespace Dynamic\Elements\Blog\Tests\Fake;
 
 use PageController as BasePageController;
 use SilverStripe\Dev\TestOnly;
-use SilverStripe\ORM\ArrayList;
-use SilverStripe\ORM\PaginatedList;
+use SilverStripe\Model\List\ArrayList;
+use SilverStripe\Model\List\PaginatedList;
 
 /**
  * Test only class used to test that the Overview block can be used outside of a Blog page
